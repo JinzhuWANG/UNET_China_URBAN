eccf7656549a8899c028cf4812005a21db23a5e6
JinzhuWANG <wangjinzhulala@gmail.com>
2024-01-08 17:20:26 +1100

    reproject big GEOTIFF

--- STEP_01_dataprep.py
+++ STEP_01_dataprep.py
@@ -1,59 +1,63 @@
 import os
 from glob import glob
 import rasterio
+from tqdm.auto import tqdm
 
-from tools import GEOTIF2HDF, create_empty_raster, remove_out_bounds_pts, reproject_raster
+from tools import GEOTIF2HDF, create_empty_raster, mosaic_tif_2_vrt, \
+                  remove_out_bounds_pts, reproject_raster, vrt2tif
 
+
+
+#####################################################
+#                  Convert LUCC to HDF              #
+#####################################################
+ 
 
 # Get LUCC GeoTIFF files 
 LUCC_files = glob('data/raster/*.tif')
-
 # Convert LUCC GEOTIFF files to HDF
-for file in LUCC_files:
-    # skip if the file is already converted
-    if os.path.isfile(file.replace('.tif', '.hdf')):
-        print('Skip existing: ', file)
-        continue
-    else:
-        GEOTIF2HDF(file)
+GEOTIF2HDF(LUCC_files)
+
         
+#####################################################
+#           Mosaic Terrain tifs to VRT              #
+#####################################################
+
+# List of raster files to mosaic
+raster_files = glob('data/raster/China_DEM_SLOPE/*.tif') 
+# Path for the output VRT file
+terrain_vrt_path = 'data/raster/terrain.vrt'
+terrain_tif_path = 'data/raster/terrain.tif'
+
+mosaic_tif_2_vrt(raster_files, terrain_vrt_path)
+vrt2tif(terrain_vrt_path, terrain_tif_path)
 
 
 
-# # Built a VRT from the DEM tiles
-# from osgeo import gdal
-
-# # List of raster files to mosaic
-# raster_files = glob('data/raster/China_DEM_SLOPE/*.tif') 
-
-# # Path for the output VRT file
-# vrt_path = 'data/raster/terrain.vrt'
-
-# # Create a VRT
-# vrt = gdal.BuildVRT(vrt_path, raster_files)
-# vrt = None  # This will close and save the VRT
+#####################################################
+#         Reproject Terrain VRT to LUCC.crs         #
+#####################################################
 
 
-
-
-import rasterio
-import pyproj
-
-# Get the destination CRS 
+# Get input/output files 
 ref_file = LUCC_files[0]
-dst_crs = pyproj.CRS(rasterio.open(ref_file).crs)
+dst_file = 'data/raster/terrain_proj2ref.tif'
 
-# Reproject the VRT to the destination CRS
-src_file = 'data/raster/terrain.vrt'
-dst_file = 'data/raster/terrain_proj2ref.hdf'
-
-create_empty_raster(src_file, dst_file, dst_crs, resolution=30, chunk_size=512)
-reproject_raster(src_file, dst_file, dst_crs)
-
-# Get the top-left coordinate of sample tiles
-ROI_box = 'data/vector/China_ROI_rect_sub_10k.shp'
-sample_top_left = remove_out_bounds_pts(LUCC_files[0], ROI_box)
+reproject_raster(terrain_tif_path, ref_file, dst_file)
 
 
 
+
+
+
+
+
+
+
+# create_empty_raster(src_file, dst_file, dst_crs, resolution=30, chunk_size=512)
+# reproject_raster(src_file, dst_file, dst_crs)
+
+# # Get the top-left coordinate of sample tiles
+# ROI_box = 'data/vector/China_ROI_rect_sub_10k.shp'
+# sample_top_left = remove_out_bounds_pts(LUCC_files[0], ROI_box)
 
--- tools/__init__.py
+++ tools/__init__.py
@@ -2,12 +2,18 @@ import os
 import geopandas as gpd
 import h5py
 import numpy as np
+from osgeo import gdal
 import pyproj
 import rasterio
 from rasterio.enums import Resampling
 from rasterio.warp import Resampling, calculate_default_transform, reproject
+from rasterio.enums import Resampling
+from rasterio.vrt import WarpedVRT
+
 from shapely.geometry import box
 from tqdm.auto import tqdm
+
+
 
 
 # Set up working directory
@@ -15,57 +21,62 @@ if __name__ == '__main__':
     os.chdir('../')
 
 
-def GEOTIF2HDF(file):
+def GEOTIF2HDF(files):
     """
     Convert GeoTIFF files to HDF5 format.
 
     Args:
-        file (str): The path to the GeoTIFF file.
+        files (list): List of file paths to GeoTIFF files.
 
     Returns:
         None
     """
-    # Convert GeoTIFF files to HDF5, keep the chunk size the same as the raster window size
-    with rasterio.open(file,'r') as src:
+    for file in files:
+        # skip if the file is already converted
+        if os.path.isfile(file.replace('.tif', '.hdf')):
+            print('Skip existing: ', file)
+        else:
+            # Convert GeoTIFF files to HDF5, keep the chunk size the same as the raster window size
+            with rasterio.open(file,'r') as src:
 
-        # Get the base name of the file
-        file_name = os.path.basename(file).split('.')[0]
+                # Get the base name of the file
+                file_name = os.path.basename(file).split('.')[0]
 
-        # Get raster info
-        tif_band_num = src.count
-        tif_shape = (tif_band_num,*src.shape)
-        tif_dtype = src.dtypes[0]
-        # Get block_windows
-        block_windows = [window for ij, window in src.block_windows()]
-        block_size = (tif_band_num,*src.block_shapes[0])
+                # Get raster info
+                tif_band_num = src.count
+                tif_shape = (tif_band_num,*src.shape)
+                tif_dtype = src.dtypes[0]
+                # Get block_windows
+                block_windows = [window for ij, window in src.block_windows()]
+                block_size = (tif_band_num,*src.block_shapes[0])
 
-        # Report the metadata of the file
-        print(f'File name: {file_name}')
-        print(f'Band number: {tif_band_num}')
-        print(f'Raster dtype: {tif_dtype}')
-        print(f'Raster shape: {tif_shape}')
-        print(f'Block size: {block_size}')
+                # Report the metadata of the file
+                print(f'File name: {file_name}')
+                print(f'Band number: {tif_band_num}')
+                print(f'Raster dtype: {tif_dtype}')
+                print(f'Raster shape: {tif_shape}')
+                print(f'Block size: {block_size}')
 
 
-        # Create HDF5 file
-        with h5py.File(fr'data/raster/{file_name}.hdf','w') as dst:
+                # Create HDF5 file
+                with h5py.File(fr'data/raster/{file_name}.hdf','w') as dst:
 
-            # Create dataset
-            dst.create_dataset('array', shape=tif_shape, dtype=tif_dtype, chunks=block_size, compression='gzip')
+                    # Create dataset
+                    dst.create_dataset('array', shape=tif_shape, dtype=tif_dtype, chunks=block_size, compression='gzip')
 
-            # Write data to dataset
-            for i, window in tqdm(enumerate(block_windows),total=len(block_windows)):
+                    # Write data to dataset
+                    for i, window in tqdm(enumerate(block_windows),total=len(block_windows)):
 
-                # Read data from GeoTIFF
-                data = src.read(window=window)
+                        # Read data from GeoTIFF
+                        data = src.read(window=window)
 
-                # Write data to HDF5
-                row_slice = slice(window.row_off, window.row_off + window.height)
-                col_slice = slice(window.col_off, window.col_off + window.width)
-                dst['array'][slice(None),row_slice, col_slice] = data
+                        # Write data to HDF5
+                        row_slice = slice(window.row_off, window.row_off + window.height)
+                        col_slice = slice(window.col_off, window.col_off + window.width)
+                        dst['array'][slice(None),row_slice, col_slice] = data
 
-        # Report a successful conversion message
-        print(f'File {file_name} converted successfully!')
+                # Report a successful conversion message
+                print(f'File {file_name} converted successfully!')
 
 
 def remove_out_bounds_pts(ref_tif, ROI_box):
@@ -159,9 +170,9 @@ def reproject_raster(src_file, dst_file, dst_crs):
         blocks = list(src.block_windows())
 
         # Read the source raster block by block
-        for ji, window in tqdm(blocks,total=len(blocks)):
+        for _, window in tqdm(blocks,total=len(blocks)):
             src_data = src.read(window=window)
-            src_crs = pyproj.CRS(src.crs)
+            src_crs = src.crs
             src_transform = src.window_transform(window)
             src_width = window.width
             src_height = window.height
@@ -174,7 +185,7 @@ def reproject_raster(src_file, dst_file, dst_crs):
             # Reproject the source raster to the destination raster   
             dst_data = np.zeros((src.count, dst_height, dst_width),dtype=src_data.dtype)
 
-            dst_data_proj,dst_data_trans = reproject(
+            dst_data_proj,_ = reproject(
                 source=src_data,
                 destination=dst_data,
                 src_transform=src_transform,
@@ -195,3 +206,88 @@ def reproject_raster(src_file, dst_file, dst_crs):
                 # Update the row and column accumulators
                 row_accumulator += dst_height
                 col_accumulator += dst_width
+
+
+def mosaic_tif_2_vrt(raster_files, vrt_path):
+    
+    if os.path.isfile(vrt_path):
+        print('Skip existing: ', vrt_path)
+        
+    else:
+        # Create a VRT
+        vrt = gdal.BuildVRT(vrt_path, raster_files)
+        vrt = None  # This will close and save the VRT
+
+
+def vrt2tif(vrt_path, tif_path):
+    """
+    Convert a VRT file to a GeoTIFF file.
+
+    Parameters:
+    vrt_path (str): The path to the VRT file.
+    dst_path (str): The path to save the GeoTIFF file.
+
+    Returns:
+    None
+    """
+    if os.path.isfile(tif_path):
+        print('Skip existing: ', tif_path)
+    else:
+        # Write the VRT to a TIF
+        with rasterio.open(vrt_path) as src:
+            
+            profile = src.profile
+            # Update the driver to GTiff, and add compression
+            profile.update(driver='GTiff',
+                           compress='lzw',
+                           BIGTIFF = "IF_SAFER")
+
+            with rasterio.open(tif_path, 'w', **profile) as dst:
+                # Iterate through blocks
+                windows = list(src.block_windows())
+                for _, window in tqdm(windows,total=len(windows)):
+                    # Read and write the data in each block
+                    data = src.read(window=window)
+                    dst.write(data, window=window)
+        print('File converted successfully!')
+        
+        
+        
+def reproject_raster(src_file, ref_file, dst_file):
+    """
+    Reprojects a raster file to match the coordinate reference system (CRS) of a reference file.
+
+    Args:
+        ref_file (str): The path to the reference file with the desired CRS.
+        dst_file (str): The path to the output file where the reprojected raster will be saved.
+
+    Returns:
+        None
+    """
+
+    if os.path.exists(dst_file):
+        print(f'Skip existing: {dst_file}')
+    else:
+    
+        # Get the CRS of the reference file
+        dst_crs = rasterio.open(ref_file).crs
+        block_size = rasterio.open(ref_file).block_shapes[0][0]
+
+        # Reproject the raster file
+        with rasterio.open(src_file) as src:
+            # Create a WarpedVRT instance
+            with WarpedVRT(src, crs=dst_crs, resampling=Resampling.nearest) as vrt:
+                profile = vrt.profile
+                profile.update(driver='GTiff',
+                            blockxsize=block_size, 
+                            blockysize=block_size,
+                            compression='lzw')
+                # Write the reprojected raster to disk
+                with rasterio.open(dst_file, 'w', **profile) as dst:
+                    # Loop through the blocks
+                    windows = list(vrt.block_windows())
+                    for ji, window in tqdm(windows,total=len(windows)):
+                        vrt_array = vrt.read(window=window)
+                        # Write each band separately
+                        for band_index in range(1, vrt.count + 1):
+                            dst.write(vrt_array, window=window)
